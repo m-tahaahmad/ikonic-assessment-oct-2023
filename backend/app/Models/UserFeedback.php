fca94cc40e594d6286b0b201da77717a00b80ef6
M. Taha Ahmad <tahaahmad929@gmail.com>
2023-10-13 09:37:24 +0500

    Base setup completed

--- backend/app/Models/UserFeedback.php
+++ backend/app/Models/UserFeedback.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class UserFeedback extends Model
 {
     use HasFactory;
 }
